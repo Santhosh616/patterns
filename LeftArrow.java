a911ffd8916200090c6434e5a0ac82519091dec2
SANTHOSH V S <151340441+Santhosh616@users.noreply.github.com>
2025-02-03 11:28:24 +0530

    Add files via upload

---
+++ LeftArrow.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LeftArrow
+{
+	public static void main(String[] args) {
+	    Scanner ip=new Scanner(System.in);
+	    int n=ip.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int sp=1;sp<=n-i;sp++){
+	            System.out.print("  ");
+	        }
+	        for(int j=1;j<=i;j++){
+	            System.out.print("* ");
+	        }
+	        System.out.println(" ");
+	    }
+	    for(int i=n-1;i>=1;i--){
+	        for(int sp=1;sp<=n-i;sp++){
+	            System.out.print("  ");
+	        }
+	        for(int j=1;j<=i;j++){
+	            System.out.print("* ");
+	        }
+	        System.out.println(" ");
+	    }
+	
+	}
+}
