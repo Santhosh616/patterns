c8594d3a40e343369460c9be387017f810b32052
SANTHOSH V S <151340441+Santhosh616@users.noreply.github.com>
2025-02-03 11:21:47 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Triangle
+{
+	public static void main(String[] args) {
+	    Scanner ip=new Scanner(System.in);
+	    int n=ip.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int sp=1;sp<=n-i;sp++){
+	            System.out.print("  ");
+	        }
+	        for(int j=1;j<=(2*i)-1;j++){
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	}
+}	
