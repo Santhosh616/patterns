16b22510b4c4d9344886b1a4f585900825e79609
SANTHOSH V S <151340441+Santhosh616@users.noreply.github.com>
2025-02-03 11:13:00 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Rectangle
+{
+	public static void main(String[] args) {
+	    Scanner ip=new Scanner(System.in);
+	    int n=ip.nextInt();
+	    int m=ip.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=m;j++){
+	            System.out.print("x ");
+	        }
+	        System.out.println(" ");
+	    }
+	
+	}
+}
